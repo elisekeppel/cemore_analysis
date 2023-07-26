6433a7684298778c9931722df634e76de680a8bc
EliseKeppel <Elise.Keppel@dfo-mpo.gc.ca>
2023-07-26 14:37:31 -0700

    tidy up

--- temp/netcdf.R
+++
@@ -1,9 +0,0 @@
-library(RNetCDF)
-
-x <- open.nc("covariates/noaa/NOAAGlobalTemp_v5.0.0_gridded_s188001_e202205_c20220608T133245.nc")
-
-
-x <- readOGR("covariates/us contacts/pacunski/All_habmaps3.shp")
-sf <- x %>% st_as_sf()
-plot(x)
-ggplot() + geom_sf(data=sf)
